852b66a1a331a1c71936af251517e655769b4ada
OhYeah229 <aaron.fuller@protonmail.com>
2019-05-22 21:21:40 -0400

    git app.js template set

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from "react-dom";
+import SearchBar from "./components/SearchBar";
+import PostContainer from "./components/PostContainer";
+import CommentSection from "./components/CommentSection";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SearchBar />
+        <PostContainer />
+        <CommentSection />
+
       </header>
     </div>
   );
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
